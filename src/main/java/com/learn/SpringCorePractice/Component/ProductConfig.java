70731b353c6b9e42e8c102ec50c10e087988b64b
unknown <chogalesanika8@gmail.com>
2024-11-11 19:59:33 +0530

    first commit

--- src/main/java/com/learn/SpringCorePractice/Component/ProductConfig.java
+++ src/main/java/com/learn/SpringCorePractice/Component/ProductConfig.java
@@ -1,8 +1,10 @@
 package com.learn.SpringCorePractice.Component;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan(basePackages = "com.learn.SpringCorePractice.Component")
 public class ProductConfig {
 
 }
