b0df40d1818430d418fd2008078d5dd3279fbcfc
unknown <chogalesanika8@gmail.com>
2024-11-11 20:01:05 +0530

    first commit

--- src/main/java/com/learn/SpringCorePractice/Component/ProductMain.java
+++ src/main/java/com/learn/SpringCorePractice/Component/ProductMain.java
@@ -1,8 +1,14 @@
 package com.learn.SpringCorePractice.Component;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class ProductMain {
 	
 	public static void main(String[] args) {
+		
+		ApplicationContext context=
+		new AnnotationConfigApplicationContext();
 		
 	}
 
