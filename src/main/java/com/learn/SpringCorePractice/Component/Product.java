dd6ba648564ed0cd9f59bf4841512bc6ba586e35
unknown <chogalesanika8@gmail.com>
2024-11-11 19:57:45 +0530

    first commit

--- src/main/java/com/learn/SpringCorePractice/Component/Product.java
+++ src/main/java/com/learn/SpringCorePractice/Component/Product.java
@@ -30,6 +30,12 @@ public class Product {
 	public String toString() {
 		return "Product [id=" + id + ", name=" + name + ", price=" + price + "]";
 	}
+	public Product(int id, String name, int price) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.price = price;
+	}
 	
 	
 	
