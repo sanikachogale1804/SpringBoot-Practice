3f4ed910bfe19b141160f6bcbdb3ae36de256d96
unknown <chogalesanika8@gmail.com>
2024-11-11 19:52:28 +0530

    first commit

--- src/main/java/com/learn/SpringCorePractice/Component.java
+++
@@ -1,8 +0,0 @@
-package com.learn.SpringCorePractice;
-
-
-public class Component {
-
-	
-	
-}
---
+++ src/main/java/com/learn/SpringCorePractice/Component/Product.java
@@ -0,0 +1,10 @@
+package com.learn.SpringCorePractice.Component;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Product {
+
+	
+	
+}
