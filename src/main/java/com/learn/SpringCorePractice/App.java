7f63a853b962534fda1e520fff6a83b0d3382adf
unknown <chogalesanika8@gmail.com>
2024-11-11 19:43:18 +0530

    first commit

---
+++ src/main/java/com/learn/SpringCorePractice/App.java
@@ -0,0 +1,13 @@
+package com.learn.SpringCorePractice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
